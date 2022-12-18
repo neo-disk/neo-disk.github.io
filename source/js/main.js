647d1b4d6c4a623d7058eeafbd1606eeb2f9a7d8
Vasiliy Zorin <vpzorin@avito.ru>
2022-12-18 15:26:03 +0400

    responsive video loading

--- source/js/main.js
+++ source/js/main.js
@@ -8,7 +8,7 @@ import {initCustomMap} from './modules/init-custom-map';
 import {initDealersList} from './modules/init-dealers-list';
 import {initYandexMap} from './modules/init-yandex-map';
 import {initAnimation} from './modules/init-animation';
-import {initVideo} from './modules/init-video';
+import {loadVideo} from './modules/init-video';
 import {initMainMap} from './modules/init-main-map';
 import {initHeaderScroll} from './modules/init-header-scroll';
 import {initHeaderResize} from './modules/init-header-resize';
@@ -30,7 +30,7 @@ window.addEventListener('DOMContentLoaded', () => {
   mobileMenu();
   initMainSlider();
   initHeaderScroll();
-  initVideo();
+  loadVideo();
   initAdditionalMenu();
 
 
@@ -44,29 +44,8 @@ window.addEventListener('DOMContentLoaded', () => {
     initYandexMapResize();
     initManipulationWithDOM();
   });
+
+  window.addEventListener("resize", (event) => {
+    loadVideo();
+  });
 });
-
-// ---------------------------------
-
-// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
-
-// привязывайте js не на классы, а на дата атрибуты (data-validate)
-
-// вместо модификаторов .block--active используем утилитарные классы
-// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
-// .select.select--opened ❌ ---> [data-select].is-open ✔️
-
-// выносим все в дата атрибуты
-// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
-
-// для адаптивного JS используейтся matchMedia и addListener
-// const breakpoint = window.matchMedia(`(min-width:1024px)`);
-// const breakpointChecker = () => {
-//   if (breakpoint.matches) {
-//   } else {
-//   }
-// };
-// breakpoint.addListener(breakpointChecker);
-// breakpointChecker();
-
-// используйте .closest(el)
--- source/js/modules/init-video.js
+++ source/js/modules/init-video.js
@@ -1,17 +1,70 @@
-const initVideo = () => {
-  const video = document.querySelector('.video');
+const neoDiskVideoProperties = {
+  src: "video/video-main.mp4",
+  width: "814",
+  height: "457",
+  type: "video/mp4",
+  mute: "muted"
+}
 
-  if (!video) {
+function renderVideoElement(containerElement) {
+  const videoEl = document.createElement('video');
+
+  videoEl.onloadeddata = () => {
+    const loader = document.querySelector('.video__loader');
+
+    function showVideo() {
+      loader.classList.add('video__loader--hidden');
+      videoEl.classList.add('visible');
+    }
+
+    showVideo()
+  }
+  videoEl.width = neoDiskVideoProperties.width
+  videoEl.height = neoDiskVideoProperties.height
+  videoEl.muted = neoDiskVideoProperties.mute
+  // videoEl.poster= neoDiskVideoProperties.poster
+  videoEl.loop = true
+  videoEl.autoplay = true
+
+  videoEl.className = 'video__player'
+
+
+  containerElement.appendChild(videoEl)
+
+  const sourceEl = document.createElement('source');
+
+  sourceEl.type = neoDiskVideoProperties.type
+  sourceEl.src = neoDiskVideoProperties.src
+
+  videoEl.appendChild(sourceEl)
+}
+
+function loadVideo() {
+  const containerElement = document.querySelector('.video-container');
+
+  if (!containerElement) {
     return;
   }
 
-  const loader = document.querySelector('.video__loader');
-  const videoPlayer = document.querySelector('.video__player');
-  function showVideo() {
-    loader.classList.add('video__loader--hidden');
-    videoPlayer.classList.add('visible');
-  }
-  setTimeout(showVideo, 1000);
-};
+  // ожидаем что
+  // .video-container {
+  //   @include vp-767 {
+  //     display: none;
+  //   }
+  // }
+  const isMobile = getComputedStyle(containerElement).display === 'none'
 
-export {initVideo};
+  if(isMobile) {
+    return
+  }
+
+  const videoEl = document.querySelector('.video__player')
+
+  if(videoEl) {
+    return;
+  }
+
+  renderVideoElement(containerElement)
+}
+
+export { loadVideo };
